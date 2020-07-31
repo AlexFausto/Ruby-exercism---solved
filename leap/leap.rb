9bba8375868d645e64e9acd0106dd5ecdf8dca69
AlexFausto <wallachianmagic@gmail.com>
2020-07-31 12:12:56 +0300

    Problems, problems every day

---
+++ leap/leap.rb
@@ -0,0 +1,14 @@
+module Year
+  # simple implementation of the 
+  # if else condition described
+  def self.leap?(year)
+    if year % 400 == 0
+      return true
+    elsif year % 100 == 0 
+      return false
+    elsif year % 4 == 0
+      return true
+    else return false
+    end
+  end
+end
